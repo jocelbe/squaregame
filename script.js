f23f898d3da3a6a47490b8f8196762576a852863
jocelbe <jocelyneaja69@gmail.com>
2026-09-02 22:46:10 +0100

    I have a mistake and not finished yet the feature

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-function draw() {
-    const canvas = document.getElementById("canvas")
-    const ctx = canvas.getContext("2d")
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
 
-    ctx.fillStyle = "rgb(200 0 0)"
-    ctx.fillRect(10, 10, 50, 50)
+let raf
 
-    ctx.fillStyle = "rgb(0 0 200 / 50%)";
-    ctx.fillRect(30, 30, 50, 50);
+const player = {
+    x:30,
+    y:30,
+    vx:5,
+    vy:5,
+    width:15,
+    height:15,
+    bgcolor: "rgb(0, 67, 200)",
+    draw(){
+        ctx.fillStyle = this.bgcolor
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+
 }
 
-draw()
+ function draw() {
+    ctx.fillStyle = "rgb(255 255 255 / 30%)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    player.draw()
+    player.x += player.vx;
+    player.y += player.vy;
 
+    raf = window.requestAnimationFrame(draw);
+ }
 
+canvas.addEventListener("keydown", function(e){
+        console.log(e.code, "touche")
+        if (e.code == 'ArrowDown') {
+           raf = window.requestAnimationFrame(draw);
+        }
+})
+
+player.draw()
