b312a5fd21264181de8051142413e30088472110
jocelbe <jocelyneaja69@gmail.com>
2026-09-03 23:41:42 +0100

    feature implemented

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const player = {
     vy:5,
     width:15,
     height:15,
+    radius: 5,
     bgcolor: "rgb(0, 67, 200)",
     draw(){
         ctx.fillStyle = this.bgcolor
@@ -18,21 +19,76 @@ const player = {
 
 }
 
- function draw() {
-    ctx.fillStyle = "rgb(255 255 255 / 30%)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+ function drawDown() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     player.draw()
-    player.x += player.vx;
     player.y += player.vy;
 
-    raf = window.requestAnimationFrame(draw);
+    if (player.y + player.vy > canvas.height - player.radius || player.y + player.vy < player.radius) 
+        {
+            player.y -= player.vy;
+        }
+
+    raf = window.requestAnimationFrame(drawDown);
  }
 
-canvas.addEventListener("keydown", function(e){
+ function drawUp() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw()
+    player.y -= player.vy;
+
+    if (player.y + player.vy > canvas.height - player.radius || player.y + player.vy < player.radius) 
+        {
+            player.y += player.vy;
+        }
+
+    raf = window.requestAnimationFrame(drawUp);
+ }
+
+ function drawRight() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw()
+    player.x += player.vx;
+
+    if (player.x + player.vx > canvas.width - player.radius || player.x + player.vx < player.radius) 
+        {
+            player.x -= player.vx;
+        }
+
+    raf = window.requestAnimationFrame(drawRight);
+ }
+
+ function drawLeft() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw()
+    player.x -= player.vx;
+
+    if (player.x + player.vx > canvas.width - player.radius || player.x + player.vx < player.radius) 
+        {
+            player.x += player.vx;
+        }
+
+    raf = window.requestAnimationFrame(drawLeft);
+ }
+
+document.addEventListener("keydown", function(e){
         console.log(e.code, "touche")
         if (e.code == 'ArrowDown') {
-           raf = window.requestAnimationFrame(draw);
+           raf = window.requestAnimationFrame(drawDown);
         }
+        if (e.code == 'ArrowUp'){
+           raf = window.requestAnimationFrame(drawUp);
+        }
+        if (e.code == 'ArrowRight'){
+           raf = window.requestAnimationFrame(drawRight);
+        }
+        if (e.code == 'ArrowLeft'){
+           raf = window.requestAnimationFrame(drawLeft);
+        }
+})
+
+document.addEventListener("keyup", function(e){
+        window.cancelAnimationFrame(raf);
 })
 
 player.draw()
